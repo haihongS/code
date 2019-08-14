0945e8373c72c3abd175d42ca18341f61cf6c854
haihongs <shh_name@hotmail.com>
2019-08-14 15:08:57 +0800

    Add docker eth

--- docker-beam/beam.sh
+++ docker-beam/beam.sh
@@ -1,1 +1,1 @@
- /root/bminer -uri beam+ssl://263b3d417078ed8326373aff018c5bc9eb7a7ac37bd0845d9c0c831d4a9b743b09b.20701@beam.f2pool.com:5000  -max-temperature 60
+ /root/bminer -uri beam+ssl://2e0a5bf20ef8307f2e07248b493e55a9d3a92860746b1788163fd0ded971c880a0b.20701@beam.f2pool.com:5000  -max-temperature 65
